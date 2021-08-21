568b3572b59af110b1f40583fe201b3ed71b093b
florenciadarosacertorio <florenciacertorio@gmail.com>
2021-08-21 17:02:50 -0300

    Login y Product
    Finalizado el login y el product

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
+//const array = []
+
+const guardarDatos = () => {
+    const correo = document.getElementById("correo");
+    const contraseña = document.getElementById("contraseña");
+    const correoValor = correo.value;
+    const contraValor = contraseña.value;
+    if (correoValor && contraValor) {
+      correo.value = "";
+      contraseña.value = "";
+      //array.push({ correoValor, contraValor });
+      localStorage.setItem("correoValor", correoValor );
+      localStorage.setItem("contraValor", contraValor );
+      window.location = "index.html";
+    } else {
+      alert("Nombre y apellido no deben ser vacíos");
+    }
+  };
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    document.getElementById("boton").addEventListener("click", guardarDatos);
 });
--- js/products.js
+++ js/products.js
@@ -1,14 +1,12 @@
-const showList = (productos) => {
+/* const showList = (productos) => {
     const infoBasica = document.createElement("ul")
     for (let product of productos){
         const li_product = document.createElement("li");
-        li_product.appendChild(document.createTextNode(`${product.name} ${product.description} ${product.cost}`))
+        li_product.appendChild(document.createTextNode(`El producto es:${product.name} su descripción: ${product.description} con un costo de:${product.cost}`))
         infoBasica.appendChild(li_product);
     }
    document.body.appendChild(infoBasica)
-
 }
-
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -18,15 +16,51 @@ document.addEventListener("DOMContentLoaded", function (e) {
         if (resultObj.status === "ok")
         {
             showList(resultObj.data);
-            console.log({
-                productos: resultObj.data,
-            });
+        }
+    });
+}); */
+
+
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted"> precio: ` + category.cost +  ` $</small>
+                    </div>
+                    <p> `+ category.description +`  </p>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
         }
     });
 });
-
-
-//categoriesArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            //showCategoriesList(categoriesArray);
     
