d6d932e86be0fd655c9af9b505470590c4922200
florenciadarosacertorio <florenciacertorio@gmail.com>
2021-08-21 17:19:23 -0300

    css product
    cambios en css en la pantalla product

--- js/products.js
+++ js/products.js
@@ -30,15 +30,15 @@ function showCategoriesList(array){
         let category = array[i];
 
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
+        <div class="contenedor">
             <div class="row">
                 <div class="col-3">
                     <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted"> precio: ` + category.cost +  ` $</small>
+                        <h4 class="mb-1 nombre">`+ category.name +`</h4>
+                        <small class="cost"> <span class="precio"> Precio: </span>` + category.cost +  ` $</small>
                     </div>
                     <p> `+ category.description +`  </p>
                 </div>
